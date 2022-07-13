7abdb70012db7f89303b7eac42de7c4cbd84821e
estesthebestes <esteshunter22@gmail.com>
2022-07-12 19:44:22 -0500

    made improvements to website

--- src/App.js
+++ src/App.js
@@ -1,45 +1,53 @@
-import {useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import Searchbar from './components/Searchbar'
-import { DataContext } from './context/DataContext'
+import { useEffect, useState, useRef } from 'react';
+import Gallery from './components/Gallery';
+import Searchbar from './components/Searchbar';
+import { DataContext } from './context/DataContext';
+import { SearchContext } from './context/SearchContext';
 
 function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+	let [search, setSearch] = useState('');
+	let [message, setMessage] = useState('Search for Music!');
+	let [data, setData] = useState([]);
+	let searchInput = useRef('');
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+	const API_URL = 'https://itunes.apple.com/search?term=';
 
 	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					setData(resData.results)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
+		if (search) {
 		}
-	}, [search])
-	
+	}, [search]);
+
 	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
+		e.preventDefault();
+		const fetchData = async () => {
+			document.title = `${term} Music`;
+			const response = await fetch(API_URL + term);
+			const resData = await response.json();
+			if (resData.results.length > 0) {
+				return setData(resData.results);
+			} else {
+				return setMessage('Not Found.');
+			}
+		};
+		fetchData();
+	};
 
 	return (
-		<div>
-			<Searchbar handleSearch = {handleSearch}/>
+		<div className='App'>
+			<SearchContext.Provider
+				value={{
+					term: searchInput,
+					handleSearch: handleSearch,
+				}}
+			>
+				<Searchbar />
+			</SearchContext.Provider>
 			{message}
 			<DataContext.Provider value={data}>
 				<Gallery />
 			</DataContext.Provider>
 		</div>
-  	);
+	);
 }
 
 export default App;
